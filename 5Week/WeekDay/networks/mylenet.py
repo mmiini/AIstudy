21feb76536d767aa64048033167eeedb0cb82f0e
mmiini <pds08134@kumoh.ac.kr>
2024-06-24 18:11:04 +0900

    Add : 2개월 1WeekDay 추가

---
+++ 5Week/WeekDay/networks/mylenet.py
@@ -0,0 +1,55 @@
+import torch.nn as nn
+import torch
+
+class lenet(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+
+        #필요한 모듈 생성
+        #까먹지말고 conv 뒤에는 bn이랑 activate 함수 넣기
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=5, kernel_size=5, stride=1, padding=0),
+            nn.BatchNorm2d(6),
+            nn.ReLU()
+        )
+        self.pool1 = nn.MaxPool2d(2,2)
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1, padding=0),
+            nn.BatchNorm2d(16),
+            nn.ReLU()
+        )
+        self.pool2 = nn.MaxPool2d(2,2)
+
+        #reshpae은 모듈이라기보다는 크기를 변경해주는 기능이기때문에 함수로 forward다시
+        self.fc1 = nn.Sequential(
+            nn.Linear(400, 120),
+            nn.ReLU()
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(120,84),
+            nn.ReLU()
+        )
+        self.fc3 = nn.Sequential(
+            nn.Linear(84, num_classes),
+            nn.ReLU()
+        )
+
+        pass
+
+    def forward(self, x):
+        batch_size, channel, height, width = x.shape
+        #데이터 x가 들어옴
+        # x의 크기는 batch, 3, 32, 32 임
+        # 이미지 크기 그대로 앞쪽 모듈부터 진행됨
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.pool2(x)
+        _, channel, height, width = x.shape
+        # 중간에 fc를 태우기 전에 reshape해야하고
+        x = torch.reshape(x, (batch_size, channel*height*width))
+        # reshape 된 feature가 다시 
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return
---
+++ 5Week/WeekDay/utils/parsing.py
@@ -0,0 +1,31 @@
+import argparse
+
+def train_parse_args():
+    parser = argparse.ArgumentParser()
+    ##모델 관련
+    parser.add_argument('--input_size', type=int, default=28*28, help='모델에 들어가는 데이터의 크기입니다.')
+    parser.add_argument('--hidden_size', type=int, default=50, help='모델 hidden layer의 크기입니다.')
+    parser.add_argument('--output_size', type=int, default=10, help='모델의 출력')
+    parser.add_argument('--model_type', type=str, default='lenet', choices=['lenet', 'mlp'], help='모델')
+    #데이터 관련
+    parser.add_argument('--batch_size', type=int, default=100)
+    parser.add_argument('--dataset', type=str, default='cifar', choices=['mnist','cifar'])
+    parser.add_argument('--num_classes', type=int, default=10 )
+    #학습 관련
+    parser.add_argument('--lr', type=float, default=0.001, help='학습률')
+    parser.add_argument('--epochs', type=int, default=10, help='학습량(에폭)')
+    #폴더 세팅
+    parser.add_argument('--save_folder_name', type=str, default='save', help='저장 폴더 묶어주는 상위 폴더 이름')
+    parser = parser.parse_args()
+    return parser
+
+
+def test_parse_args():
+    parser = argparse.ArgumentParser()
+    ##어떤 모델 쓸건지
+    parser.add_argument('--example_image_path', type=str, default='mnist_example.jpeg')
+    #어떤 데이터 쓸껀지
+    parser.add_argument('--trained_folder_path', type=str, default='save/2')
+    
+    parser = parser.parse_args()
+    return parser
